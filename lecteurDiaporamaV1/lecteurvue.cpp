9b953d7cb9c109fb86f7f4d9d7f795a240837bbf
Néo ELDUAYEN <118976477+Niwowo@users.noreply.github.com>
2023-04-28 11:00:46 +0200

    Début de la V1, création de l'interface graphique

---
+++ lecteurDiaporamaV1/lecteurvue.cpp
@@ -0,0 +1,15 @@
+#include "lecteurvue.h"
+#include "ui_lecteurvue.h"
+
+LecteurVue::LecteurVue(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::LecteurVue)
+{
+    ui->setupUi(this);
+}
+
+LecteurVue::~LecteurVue()
+{
+    delete ui;
+}
+
